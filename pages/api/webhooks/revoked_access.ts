9902294acb972f63aff898a9cbe8a5f45b615e86
Jordan Bergero <jbergero@squareup.com>
2023-12-05 12:28:29 -0800

    update webhooks, env.example, and fly.io confs

--- pages/api/webhooks/revoked_access.ts
+++ pages/api/webhooks/revoked_access.ts
@@ -17,20 +17,24 @@ const isFromSquare = (body: string, signature: string):boolean => {
 }
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (!isString(req.headers['x-square-hmacsha256-signature'])) {
-        return res.status(403)
-    }
-
-    // validate that the request is from square
-    if (!isFromSquare(
-        JSON.stringify(req.body),
-        req.headers['x-square-hmacsha256-signature'])
-    ) {
-        res.status(403)
-    } else {
-        const { merchant_id } = req.body
-        await deleteSquareDataByMerchantId(merchant_id)
-        res.status(200)
+    try{
+        if (!isString(req.headers['x-square-hmacsha256-signature'])) {
+            return res.status(403).json({ error: 'hmacsha256 signature missing' })
+        }
+        // validate that the request is from square
+        if (!isFromSquare(
+            JSON.stringify(req.body),
+            req.headers['x-square-hmacsha256-signature'])
+        ) {
+            return res.status(403).json({ error: 'Invalid signature' })
+        } else {
+            const { merchant_id } = req.body
+            await deleteSquareDataByMerchantId(merchant_id)
+            return res.status(200).json({ message: 'success' })
+        }
+    } catch(e) {
+        console.error(e)
+        return res.status(500).json({ error: 'Internal server error' })
     }
 }
 
